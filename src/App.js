b863e22cd450b2428c70d5f526772a1d3dc13b36
unknown <lucased1048l@gmail.com>
2023-05-25 18:12:38 -0300

    novos arquivos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Text, View, StyleSheet} from 'react-native'
+import {Text, View, StyleSheet, DrawerLayoutAndroidComponent} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 //import Primeiro from './componentes/PrimeiroComponente'
@@ -24,67 +24,81 @@ import Quadrado from "./componentes/layout/Quadrado"
 // import ListaProdutosV2 from "./componentes/produto/ListaProdutos"
 // import ListaProdutos from "./componentes/produto/ListaProdutos"
 // import FlexboxV2 from "./componentes/layout/FlexboxV2"
-import FlexboxV3 from "./componentes/layout/FlexboxV3"
+import FlexboxV4 from "./componentes/layout/FlexboxV4"
+import Mega from "./componentes/mega/Mega"
+import AppFilmes from "./componentes/atividade/AppFilmes"
+import AppFilme2 from "./componentes/atividade/AppFilme2"
+
 
 const Stack = createNativeStackNavigator()
+const StackNavigatorContainer = NavigationContainer
 
 export default () => (
     
-    <SafeAreaView style={styles.TelaIniciante}>
-        {/* <FlexnoxV1/> */}
-        {/* <FlexboxV2/> */}
-        <FlexboxV3/>
-   {/* <Quadrado/>
-       <Quadrado cor='#836FFF'/>
-       <Quadrado cor='#C112DF'/>
-       <Quadrado cor='#0F79F2'/>
-       <Quadrado cor='#0F7334'/> */}
-        {/* <ListaProdutos/> */}
-        {/* <ListaProdutosV2/> */}
-{/* //     <Familia>
-//         <Membro nome="K" sobrenome="Pereiras"/>
-//         <Membro nome="R" sobrenome="Pereiras"/>
-//         <Membro nome="L" sobrenome="Pereiras"/>
-//         <Membro nome="H" sobrenome="Pereiras"/>
-
-//     </Familia>
-//     <Familia>
-//     <Membro nome="P" sobrenome="Da silva"/>
-//     <Membro nome="O" sobrenome="Da silva"/>
-//     <Membro nome="R" sobrenome="Da silva"/>
-//     <Membro nome="R" sobrenome="Da silva"/>
-//     <Membro nome="A" sobrenome="Da silva"/>
-// </Familia> */}
-</SafeAreaView>
-    // <NavigationContainer>
-    //         <Stack.Navigator>
-    //             <Stack.Screen name="Menu" component={Menu}/>
-    //             <Stack.Screen name="Aleatorio" component={Aleatorio}/>
-    //             <Stack.Screen name="ContadorV2" component={ContadorV2}/>
-    //             <Stack.Screen name="Telas" component={Telas}/>
-               
-        //     </Stack.Navigator>
-        // </NavigationContainer>
-    // // <View style={styles.TelaIniciante}>
-    //     {/* <ContadorV2/> */}
-    //     {/* <Pai/> */}
-    // {/* <DiasDaSemana inicial="Domingo"/> */}
-    //     {/* <Contador inicial={100}/> */}
-    //     {/* <Botao/> */}
-    //     {/*<Titulo principal="Cadastro Produto"
-    //             secundario="Tela de Cadastro"/>
-    //      <Primeiro/>
-    //     </ViewText>1 + 2</Text>
-    //     <Text>O valor da soma 1 + 2 = {1+2}</Text>
-    //     <Comp1/>
-    //     <Comp2/>
-    //     <Of/> /}
-    //     {/ <MinMax min="3" max ="20"/>
-    //     <MinMax min="5" max ="40"/>
-    //     <MinMax min="7" max ="55"/>
+    
+        // {/* <FlexnoxV1/> */}
+        // {/* <FlexboxV2/> */}
+        // {/* <FlexboxV4/> */}
+        // {/* <Mega qtdeNumeros={7}/> */}
         
-        //  {/ <Aleatorio miny={0} maxx={100} /> */}
-    // // </View>
+        <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="AppFilmes" component={AppFilmes}/>
+        <Stack.Screen name="AppFilmes2" component={AppFilme2}/>
+        </Stack.Navigator>
+        </NavigationContainer>
+        
+//    {/* <Quadrado/>
+//        <Quadrado cor='#836FFF'/>
+//        <Quadrado cor='#C112DF'/>
+//        <Quadrado cor='#0F79F2'/>
+//        <Quadrado cor='#0F7334'/> */}
+//         {/* <ListaProdutos/> */}
+//         {/* <ListaProdutosV2/> */}
+// {/* //     <Familia>
+// //         <Membro nome="K" sobrenome="Pereiras"/>
+// //         <Membro nome="R" sobrenome="Pereiras"/>
+// //         <Membro nome="L" sobrenome="Pereiras"/>
+// //         <Membro nome="H" sobrenome="Pereiras"/>
+
+// //     </Familia>
+// //     <Familia>
+// //     <Membro nome="P" sobrenome="Da silva"/>
+// //     <Membro nome="O" sobrenome="Da silva"/>
+// //     <Membro nome="R" sobrenome="Da silva"/>
+// //     <Membro nome="R" sobrenome="Da silva"/>
+// //     <Membro nome="A" sobrenome="Da silva"/>
+// // </Familia> */}
+
+//     // <NavigationContainer>
+//     //         <Stack.Navigator>
+//     //             <Stack.Screen name="Menu" component={Menu}/>
+//     //             <Stack.Screen name="Aleatorio" component={Aleatorio}/>
+//     //             <Stack.Screen name="ContadorV2" component={ContadorV2}/>
+//     //             <Stack.Screen name="Telas" component={Telas}/>
+               
+//         //     </Stack.Navigator>
+//         // </NavigationContainer>
+//     // // <View style={styles.TelaIniciante}>
+//     //     {/* <ContadorV2/> */}
+//     //     {/* <Pai/> */}
+//     // {/* <DiasDaSemana inicial="Domingo"/> */}
+//     //     {/* <Contador inicial={100}/> */}
+//     //     {/* <Botao/> */}
+//     //     {/*<Titulo principal="Cadastro Produto"
+//     //             secundario="Tela de Cadastro"/>
+//     //      <Primeiro/>
+//     //     </ViewText>1 + 2</Text>
+//     //     <Text>O valor da soma 1 + 2 = {1+2}</Text>
+//     //     <Comp1/>
+//     //     <Comp2/>
+//     //     <Of/> /}
+//     //     {/ <MinMax min="3" max ="20"/>
+//     //     <MinMax min="5" max ="40"/>
+//     //     <MinMax min="7" max ="55"/>
+        
+//         //  {/ <Aleatorio miny={0} maxx={100} /> */}
+//     // // </View>
 
 )
 
